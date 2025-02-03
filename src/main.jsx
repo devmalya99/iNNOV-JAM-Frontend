0daf729afe7d9414cb2d415f2173e1825cdfdf2a
Debmalya <debmalyamazumdar99@gmail.com>
2025-02-03 16:38:01 +0530

    removed auth0proovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -6,17 +6,12 @@ import { ThemeProvider } from "../Context/ThemeContext.jsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from 'react-query'; 
 import { AuthProvider } from "./../Context/AuthContext.jsx";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-  <Auth0Provider
-    domain="dev-1hl2rm716rj3vt6c.us.auth0.com"
-    clientId="gyOghapOk4MMKQB3YyKOb5ERKYbimWaK"
-    redirectUri={window.location.origin}
-  >
+  
   <BrowserRouter>
     
       <AuthProvider>
@@ -26,6 +21,6 @@ createRoot(document.getElementById("root")).render(
       </AuthProvider>
     
   </BrowserRouter>
-  </Auth0Provider>
+ 
   </QueryClientProvider>
 );
