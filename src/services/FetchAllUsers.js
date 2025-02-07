3e537d92add6aad5728d0ef8449203a5a48ce0d2
Debmalya <debmalyamazumdar99@gmail.com>
2025-02-07 11:23:05 +0530

    display users fi

--- src/services/FetchAllUsers.js
+++ src/services/FetchAllUsers.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 const fetchAllUsers = async () => {
-  const response = await axios.get(`${VITE_API_URL}/api/users/all`);
+  const response = await axios.get(`${VITE_API_URL}/api/users/getusers`);
   console.log(response.data)
   return response.data;
 };
@@ -18,3 +18,5 @@ export const FetchAllUsers = () => {
     refetchOnWindowFocus: true, 
   });
 };
+
+
